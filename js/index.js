27e687eea186845f575fcfef68da2d30dc0e9614
anmxploitt <135054295+anmxploitt@users.noreply.github.com>
2025-07-14 17:47:09 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Special For Inka`.split('')
+const text = `For Inka ♥️`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
